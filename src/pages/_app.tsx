91c9486d3834c3fe0a9d05d403618d23de5ca646
John Koen <jkoen@smartronix.com>
2023-05-29 13:35:04 -0400

    Cleaned up file structure, added hello sample API

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import Layout from '@/components/layout/layout';
-import '@/styles/globals.css';
 import { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
